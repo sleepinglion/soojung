aac7d70ba973fd212249a8d922ec053a2961c146
sleeping-lion <toughjjh@gmail.com>
2017-01-17 04:01:27 +0900

    update

--- db/migrate/20131109141604_create_impressions_table.rb
+++ db/migrate/20131109141604_create_impressions_table.rb
@@ -12,9 +12,11 @@ class CreateImpressionsTable < ActiveRecord::Migration
       t.string :session_hash
       t.text :message
       t.text :referrer
+      t.text :params      
       t.timestamps :null=>false
     end
-    add_index :impressions, [:impressionable_type, :message, :impressionable_id], :name => "impressionable_type_message_index", :unique => false, :length => {:message => 255 }
+    
+    add_index :impressions, [:impressionable_type, :impressionable_id, :params], :name => "poly_params_request_index", :unique => false    
     add_index :impressions, [:impressionable_type, :impressionable_id, :request_hash], :name => "poly_request_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :ip_address], :name => "poly_ip_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :session_hash], :name => "poly_session_index", :unique => false
