1d28c145c957d6b5f5827fa3fb5c70ec62580178
sleeping-lion <toughjjh@gmail.com>
2017-02-01 23:12:42 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'soojung'
-set :repo_url, 'git@github.com:sleeping-lion/soojung.git'
+set :repo_url, 'git@github.com:sleepinglion/soojung.git'
 set :branch, 'master'
 set :deploy_to, '/home/deploy/soojung'
 # set :scm, :git
@@ -8,43 +8,32 @@ set :deploy_to, '/home/deploy/soojung'
 # set :log_level, :debug
 set :pty, true
 set :linked_files, %w{config/database.yml .env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/assets}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/assets public/ckeditor}
 
-#set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
 namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+         execute :rake, 'tmp:clear'
+      end
     end
   end
-  
+
   desc 'Refresh sitemap'
   task :refresh_sitemap do
     on roles(:app), in: :sequence, wait: 1 do
       within release_path do
-        with rails_env: fetch(:rails_env) do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
           execute :rake, 'sitemap:refresh'
         end
       end
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
   after :finishing, 'deploy:refresh_sitemap'
   after :finishing, 'deploy:cleanup'
 end
-
-
