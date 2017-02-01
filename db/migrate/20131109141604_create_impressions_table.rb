5410a98c7d71f050252cfddbb99372a5436d3d0d
sleeping-lion <toughjjh@gmail.com>
2017-02-01 23:59:36 +0900

    update

--- db/migrate/20131109141604_create_impressions_table.rb
+++ db/migrate/20131109141604_create_impressions_table.rb
@@ -1,22 +1,22 @@
 class CreateImpressionsTable < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :impressions, :force => true do |t|
-      t.string :impressionable_type
+      t.string :impressionable_type, :limit=>100
       t.integer :impressionable_id
       t.integer :user_id
-      t.string :controller_name
-      t.string :action_name
-      t.string :view_name
-      t.string :request_hash
-      t.string :ip_address
-      t.string :session_hash
+      t.string :controller_name, :limit=>100
+      t.string :action_name, :limit=>100
+      t.string :view_name, :limit=>100
+      t.string :request_hash, :limit=>100
+      t.string :ip_address, :limit=>100
+      t.string :session_hash, :limit=>100
       t.text :message
       t.text :referrer
-      t.text :params      
-      t.timestamps :null=>false
+      t.string :params, :limit=>150
+      t.timestamps
     end
-    
-    add_index :impressions, [:impressionable_type, :impressionable_id, :params], :name => "poly_params_request_index", :unique => false    
+
+    add_index :impressions, [:impressionable_type, :impressionable_id, :params], :name => "poly_params_request_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :request_hash], :name => "poly_request_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :ip_address], :name => "poly_ip_index", :unique => false
     add_index :impressions, [:impressionable_type, :impressionable_id, :session_hash], :name => "poly_session_index", :unique => false
@@ -24,9 +24,5 @@ class CreateImpressionsTable < ActiveRecord::Migration
     add_index :impressions, [:controller_name,:action_name,:ip_address], :name => "controlleraction_ip_index", :unique => false
     add_index :impressions, [:controller_name,:action_name,:session_hash], :name => "controlleraction_session_index", :unique => false
     add_index :impressions, :user_id
-  end
-
-  def self.down
-    drop_table :impressions
   end
 end
